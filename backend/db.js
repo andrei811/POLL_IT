21dea328e17b30575fb403e04a33d27d7d37222c
Andrei Stan <standrei2003@yahoo.com>
2023-12-07 17:11:59 +0000

    mod devcontainer

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.log('Error connecting to MongoDB:', error);
-    }
+  try {
+    await mongoose.connect("mongodb://root:example@localhost:27017/admin");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
 };
 
 module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const connectDB = require('./db');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const connectDB = require("./db");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const PORT = 3001;
@@ -13,22 +15,24 @@ connectDB();
 app.use(express.json());
 
 // Define authentication routes
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 // Define user routes
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
 
-app.use(session({
-    secret: 'fiwafhiwfwhvuwvu9hvvvwv', // Never ever share this secret in production, keep this in separate file on environmental variable 
+app.use(
+  session({
+    secret: "fiwafhiwfwhvuwvu9hvvvwv", // Never ever share this secret in production, keep this in separate file on environmental variable
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: oneDay },
+    cookie: { maxAge: 60000000 },
     store: MongoStore.create({
-        mongoUrl: 'your_mongo_url'
-    })
-})); 
+      mongoUrl: "your_mongo_url",
+    }),
+  })
+);
